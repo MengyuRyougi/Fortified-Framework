913becf40a288574f382c7007a1e22328a43961b
GoGaTio <165080256+GoGaTio@users.noreply.github.com>
2026-03-21 10:34:37 +0300

    CompCauseGameConditionAdjustable

---
+++ _Sources/Fortified/Components/CompCauseGameConditionAdjustable.cs
@@ -0,0 +1,42 @@
+﻿using RimWorld;
+using RimWorld.Planet;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+
+namespace Fortified
+{
+	public class CompCauseGameConditionAdjustable : CompCauseGameCondition
+	{
+		protected CompFlickable flickableComp;
+
+		protected CompPowerTrader powerComp;
+
+		public override bool Active
+		{
+			get
+			{
+				if (!parent.Spawned)
+				{
+					return false;
+				}
+				if (flickableComp != null && !flickableComp.SwitchIsOn)
+				{
+					return false;
+				}
+				if (powerComp != null && !powerComp.PowerOn)
+				{
+					return false;
+				}
+				return base.Active;
+			}
+		}
+
+		public override void PostSpawnSetup(bool respawningAfterLoad)
+		{
+			flickableComp = parent.GetComp<CompFlickable>();
+			powerComp = parent.GetComp<CompPowerTrader>();
+			base.PostSpawnSetup(respawningAfterLoad);
+		}
+	}
+}
